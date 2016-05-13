ba5a72e1721b720031fc1426e19c6ab264803dcf
mospans <mospans@gmail.com>
2016-05-13 10:55:54 +0300

    Create post's page (index.php)

--- index.php
+++ index.php
@@ -3,4 +3,9 @@
  * @package WordPress
  * @subpackage blank_theme
  */
- ?>
+?>
+<?php get_header(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+	<?php the_content(); ?>
+<?php endwhile; ?>
+<?php get_footer(); ?>
